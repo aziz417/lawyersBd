1b79700d495ef053c9b9553552f1d3c80871c77a
aziz417 <aziz97cse@gmail.com>
2021-12-29 13:58:33 +0600

    about say remove

--- database/migrations/2021_12_29_042637_add_about_say_you_to_registrations_table.php
+++ database/migrations/2021_12_29_042637_add_about_say_you_to_registrations_table.php
@@ -14,13 +14,10 @@ class AddAboutSayYouToRegistrationsTable extends Migration
     public function up()
     {
         Schema::table('registrations', function (Blueprint $table) {
-            $table->text('about_say_you')->nullable()->default(`stare decisis: (stah-ree
-            duh-sigh-sis) n. Latin for "to stand by a decision," the doctrine that a trial
-            court is bound by appellate court decisions (precedents) on a legal question
-            which is raised in the lower court. Reliance on such precedents is required
+            $table->text('about_say_you')->nullable()->default(`Reliance on such precedents is required
             of trial courts until such time as an appellate court changes the rule, for
             the trial court cannot ignore the precedent (even when the trial judge
-            believes it is "bad law").`);
+            believes it is`);
         });
     }
 
