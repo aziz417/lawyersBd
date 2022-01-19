89cc64eba35fb163dfe790803e1daf771609b56d
aziz417 <aziz97cse@gmail.com>
2022-01-19 14:07:03 +0600

    fds

--- resources/views/frontend/pages/LawyerList.blade.php
+++ resources/views/frontend/pages/LawyerList.blade.php
@@ -59,6 +59,8 @@
     @include('frontend.components.hire-now-modal', $types = $caseTypes)
 </section>
 @endsection
+
+
 @section('script')
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
