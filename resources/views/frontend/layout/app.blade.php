2a38e3c007399812733286f2c95fc76a93321d26
aziz417 <aziz97cse@gmail.com>
2022-01-26 21:32:08 +0600

    title set

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Texas Lawers</title>
+    <title>Find Lawyer</title>
 
     @include('frontend.layout.includes.all-css')
     @yield('style')
